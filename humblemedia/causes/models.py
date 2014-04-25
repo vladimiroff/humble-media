cf3596ee93eabf425c7d42c15fc07b11f7741158
victoria92 <viki.hristova@gmail.com>
2014-04-26 00:54:12 +0300

    Add organization to cause model

--- humblemedia/causes/models.py
+++ humblemedia/causes/models.py
@@ -5,6 +5,7 @@ class Cause(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
     creator = models.ForeignKey('auth.User', related_name='causes')
+    organization = models.ForeignKey('organizations.Organization', related_name='causes', null=True, blank=True)
     target = models.PositiveIntegerField(null=True, blank=True)
     url = models.URLField(null=True, blank=True)
     is_verified = models.BooleanField(default=False)
