a25aeb48278860cc290be30b8a8b5c895e4bcd89
Ivaylo Bachvarov <bachvarof@gmail.com>
2014-04-26 00:17:35 +0300

    Add base home structure

--- humblemedia/humblemedia/urls.py
+++ humblemedia/humblemedia/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'humblemedia.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'humblemedia.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
