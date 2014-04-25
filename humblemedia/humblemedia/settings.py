6b1c525d707b697c1d160dbff19ebabda8a5297a
Kiril Vladimirov <kiril@vladimiroff.org>
2014-04-25 22:08:56 +0300

    Create the initial django project

---
+++ humblemedia/humblemedia/settings.py
@@ -0,0 +1,47 @@
+import os
+
+ALLOWED_HOSTS = []
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DEBUG = True
+LANGUAGE_CODE = 'en-us'
+ROOT_URLCONF = 'humblemedia.urls'
+SECRET_KEY = '5*trcgvuqlu)c+n2665!*-m=xicn$tuitwy4w7rknelpreleg!'
+STATIC_URL = '/static/'
+TEMPLATE_DEBUG = True
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+WSGI_APPLICATION = 'humblemedia.wsgi.application'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django_extensions',
+
+    'accounts',
+    'payments',
+    'contents',
+    'causes',
+    'organizations',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+try:
+    from .local_settings import *
+except ImportError:
+    exit("local_settings.py NOT found!")
