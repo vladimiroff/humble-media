173555bf9073cebb07655e34516e3e464de02feb
victoria92 <viki.hristova@gmail.com>
2014-04-25 23:50:19 +0300

    Add cause model

--- humblemedia/causes/models.py
+++ humblemedia/causes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Cause(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    creator = models.ForeignKey('auth.User', related_name='causes')
+    target = models.PositiveIntegerField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
