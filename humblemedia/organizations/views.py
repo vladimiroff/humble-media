628adc8bc1b3ff79ba94c4465d880855ad414bfd
victoria92 <viki.hristova@gmail.com>
2014-04-26 12:44:56 +0300

    Add functionality for adding organization

--- humblemedia/organizations/views.py
+++ humblemedia/organizations/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Organization
+from .forms import OrganizationForm
+
+@login_required
+def add_organization(request):
+    data = request.POST if request.POST else None
+    form = OrganizationForm(data, user=data['admins'][0])
+    if form.is_valid():
+        form.save()
+        return redirect('organizations/{}'.format(form.instance.pk))
+    return render(request, 'organization/add.html', locals())
