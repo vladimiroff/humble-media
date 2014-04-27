625f6ea4db1eb6d5f44d67856049cb5000eb5986
Kiril Vladimirov <kiril@vladimiroff.org>
2014-04-27 15:15:56 +0300

    Serve the latest resources of all kinds on the homepage

--- humblemedia/humblemedia/views.py
+++ humblemedia/humblemedia/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from resources.models import Resource
+
 
 def home(request):
+    audios = Resource.audios.all()[:5]
+    documents = Resource.documents.all()[:5]
+    images = Resource.images.all()[:5]
+    videos = Resource.videos.all()[:5]
+    others = Resource.others.all()[:5]
     return render(request, "home.html", locals())
